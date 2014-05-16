2809d2dc735a71baa6aaea7a93a6f9fc0081dcb0
Nick Walker <nwalker75@yahoo.com>
2014-05-16 10:19:36 -0600

    first commit. test driven development.

---
+++ main.js
@@ -0,0 +1,15 @@
+// $(document).on('ready', function() {
+  
+// });
+
+// Returns a string that concatenates each string from the array separated by 
+// the given delimeter.
+// e.g. join(['one', 'two', 'three'], ' and ') 
+// returns: 'one and two and three'
+var join = function(arr, delimeter) {
+ var output = '';
+ for(var i=0; i<arr.length; i++) {
+  output += arr[i] + delimeter;
+ }
+ return output;
+};
---
+++ test/spec.js
@@ -0,0 +1,27 @@
+// describe("Module", function() {
+//   it("should do something", function() {
+//     expect(true).toEqual(false);
+//   });
+// });
+
+
+// Returns a string that concatenates each string from the array separated by 
+// the given delimeter.
+// e.g. join(['one', 'two', 'three'], ' and ') 
+// returns: 'one and two and three'
+// var join = function(arr, delimeter) {
+//  var output = '';
+//  for(var i=0; i<arr.length; i++) {
+//   output += arr[i] + delimeter;
+//  }
+//  return output;
+// };
+
+
+describe('join', function() {
+	it('should return a string with the give', function() {
+		expect(join('red', 'white', 'blue')).toEqual()
+	}
+})
+
+join(['red', 'white', 'blue'], ' and ')
